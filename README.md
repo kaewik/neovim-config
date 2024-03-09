d6fc4ac39e7f7d4995033c97f4df819e1a620cc3
kaewik <32685131+kaewik@users.noreply.github.com>
2024-03-09 22:02:13 +0100

    Add typst support

--- README.md
+++ README.md
@@ -3,6 +3,15 @@
 This is configuration is powered by this awesome [neovim-flake](https://github.com/jordanisaacs/neovim-flake).
 Still, it is based on my own fork, s.th. I have the freedom to change things, see [my neovim-flake fork](https://github.com/kaewik/neovim-flake).
 
+## Prerequisites
+
+- nix package manager
+- jq
+- /etc/nix.conf needs to have the following line:
+```
+experimental-features = nix-command flakes
+```
+
 ## How to install?
 
 Just run `./install.sh`.
--- flake.nix
+++ flake.nix
@@ -1,4 +1,5 @@
 {
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   inputs.neovim-flake.url = "github:kaewik/neovim-flake";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
@@ -13,113 +14,114 @@
     neovim-flake,
     copilot,
     ...
-  }: flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        configModule = {
-          # Add any custom options (and feel free to upstream them!)
-          # options = ...
-          config.build = {
-            viAlias = false;
-            vimAlias = true;
-            rawPlugins = {
-              copilot.src = copilot;
-            };
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      configModule = {
+        # Add any custom options (and feel free to upstream them!)
+        # options = ...
+        config.build = {
+          viAlias = false;
+          vimAlias = true;
+          rawPlugins = {
+            copilot.src = copilot;
           };
-          config.vim = {
-            startPlugins = ["copilot"];
-            disableArrows = true;
-            scrollOffset = 999;
-            languages = {
-              enableLSP = false;
-              enableFormat = true;
-              enableTreesitter = true;
-              enableExtraDiagnostics = false;
+        };
+        config.vim = {
+          startPlugins = ["copilot"];
+          disableArrows = true;
+          scrollOffset = 999;
+          languages = {
+            enableLSP = false;
+            enableFormat = true;
+            enableTreesitter = true;
+            enableExtraDiagnostics = false;
 
-              bash.enable = true;
-              nix.enable = true;
-              markdown.enable = true;
-              html.enable = true;
-              clang.enable = true;
-              sql.enable = true;
-              rust = {
-                enable = true;
-                crates.enable = true;
-              };
-              ts.enable = true;
-              go.enable = true;
-              zig.enable = true;
-              python.enable = true;
-              plantuml.enable = true;
-              java.enable = true;
-              kotlin.enable = true;
-              terraform.enable = true;
-              cmake.enable = true;
+            bash.enable = true;
+            nix.enable = true;
+            markdown.enable = true;
+            html.enable = true;
+            clang.enable = true;
+            sql.enable = true;
+            rust = {
+              enable = true;
+              crates.enable = true;
+            };
+            ts.enable = true;
+            go.enable = true;
+            zig.enable = true;
+            python.enable = true;
+            plantuml.enable = true;
+            java.enable = true;
+            kotlin.enable = true;
+            terraform.enable = true;
+            cmake.enable = true;
+            typst.enable = true;
 
-              # See tidal config
-              tidal.enable = false;
-            };
-            lsp = {
-              formatOnSave = false;
-              lspkind.enable = false;
-              lightbulb.enable = false;
-              lspsaga.enable = false;
-              nvimCodeActionMenu.enable = false;
-              trouble.enable = false;
-              lspSignature.enable = false;
-            };
-            visuals = {
+            # See tidal config
+            tidal.enable = false;
+          };
+          lsp = {
+            formatOnSave = false;
+            lspkind.enable = false;
+            lightbulb.enable = false;
+            lspsaga.enable = false;
+            nvimCodeActionMenu.enable = false;
+            trouble.enable = false;
+            lspSignature.enable = false;
+          };
+          visuals = {
+            enable = true;
+            nvimWebDevicons.enable = true;
+            indentBlankline = {
               enable = true;
-              nvimWebDevicons.enable = true;
-              indentBlankline = {
-                enable = true;
-                fillChar = null;
-                eolChar = null;
-                showCurrContext = true;
-              };
-              cursorWordline = {
-                enable = true;
-                lineTimeout = 0;
-              };
+              fillChar = null;
+              eolChar = null;
+              showCurrContext = true;
             };
-            statusline.lualine.enable = true;
-            theme.enable = true;
-            autopairs.enable = true;
-            autocomplete = {
+            cursorWordline = {
               enable = true;
-              type = "nvim-cmp";
-            };
-            filetree.nvimTreeLua = {
-              enable = true;
-              treeWidth = 500;
-              hideFiles = [ ];
-              disableNetRW = true;
-              closeOnFileOpen = true;
-            };
-            tabline.nvimBufferline.enable = true;
-            treesitter.context.enable = true;
-            keys = {
-              enable = true;
-              whichKey.enable = true;
-            };
-            telescope.enable = true;
-            git = {
-              enable = true;
-              gitsigns.enable = true;
-              gitsigns.codeActions = true;
+              lineTimeout = 0;
             };
           };
-        };
-      in {
-        packages = rec {
-          customNeovim = neovim-flake.lib.neovimConfiguration {
-            modules = [configModule];
-            inherit pkgs;
+          statusline.lualine.enable = true;
+          theme.enable = true;
+          autopairs.enable = true;
+          autocomplete = {
+            enable = true;
+            type = "nvim-cmp";
           };
-          neovim = customNeovim;
+          filetree.nvimTreeLua = {
+            enable = true;
+            treeWidth = 500;
+            hideFiles = [];
+            disableNetRW = true;
+            closeOnFileOpen = true;
+          };
+          tabline.nvimBufferline.enable = true;
+          treesitter.context.enable = true;
+          keys = {
+            enable = true;
+            whichKey.enable = true;
+          };
+          telescope.enable = true;
+          git = {
+            enable = true;
+            gitsigns.enable = true;
+            gitsigns.codeActions = true;
+          };
         };
-        apps = rec {
-          neovim = flake-utils.lib.mkApp { drv = self.packages.${system}.neovim; };
+      };
+    in {
+      packages = rec {
+        customNeovim = neovim-flake.lib.neovimConfiguration {
+          modules = [configModule];
+          inherit pkgs;
         };
-      });
+        neovim = customNeovim;
+      };
+      apps = rec {
+        neovim = flake-utils.lib.mkApp {drv = self.packages.${system}.neovim;};
+      };
+    });
 }
