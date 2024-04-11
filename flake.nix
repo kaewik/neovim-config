fdb54254fa0c0925fa39b03baa662231918dee77
kaewik <32685131+kaewik@users.noreply.github.com>
2024-04-11 16:39:48 +0200

    Provide dummy config

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
   inputs.neovim-flake.url = "github:kaewik/neovim-flake";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
@@ -33,88 +34,91 @@
           };
         };
         config.vim = {
-          startPlugins = ["copilot" "vim-sleuth"];
-          disableArrows = true;
-          scrollOffset = 999;
-          languages = {
-            enableLSP = false;
-            enableFormat = true;
-            enableTreesitter = true;
-            enableExtraDiagnostics = false;
+        luaConfigRC.myFancyLuaConfig = neovim-flake.lib.nvim.dag.entryAnywhere /* lua */ ''
+          vim.fn.printf("Hello World!")
+        '';
+        startPlugins = ["copilot" "vim-sleuth"];
+        disableArrows = true;
+        scrollOffset = 999;
+        languages = {
+          enableLSP = false;
+          enableFormat = true;
+          enableTreesitter = true;
+          enableExtraDiagnostics = false;
 
-            bash.enable = true;
-            nix.enable = true;
-            markdown.enable = true;
-            html.enable = true;
-            clang.enable = true;
-            sql.enable = true;
-            rust = {
-              enable = true;
-              crates.enable = true;
-            };
-            ts.enable = true;
-            go.enable = true;
-            zig.enable = true;
-            python.enable = true;
-            plantuml.enable = true;
-            java.enable = true;
-            kotlin.enable = true;
-            terraform.enable = true;
-            cmake.enable = true;
-            typst.enable = true;
+          bash.enable = true;
+          nix.enable = true;
+          markdown.enable = true;
+          html.enable = true;
+          clang.enable = true;
+          sql.enable = true;
+          rust = {
+            enable = true;
+            crates.enable = true;
+          };
+          ts.enable = true;
+          go.enable = true;
+          zig.enable = true;
+          python.enable = true;
+          plantuml.enable = true;
+          java.enable = true;
+          kotlin.enable = true;
+          terraform.enable = true;
+          cmake.enable = true;
+          typst.enable = true;
 
-            # See tidal config
-            tidal.enable = false;
-          };
-          lsp = {
-            formatOnSave = false;
-            lspkind.enable = false;
-            lightbulb.enable = false;
-            lspsaga.enable = false;
-            nvimCodeActionMenu.enable = false;
-            trouble.enable = false;
-            lspSignature.enable = false;
-          };
-          visuals = {
+# See tidal config
+          tidal.enable = false;
+        };
+        lsp = {
+          formatOnSave = false;
+          lspkind.enable = false;
+          lightbulb.enable = false;
+          lspsaga.enable = false;
+          nvimCodeActionMenu.enable = false;
+          trouble.enable = false;
+          lspSignature.enable = false;
+        };
+        visuals = {
+          enable = true;
+          nvimWebDevicons.enable = true;
+          indentBlankline = {
             enable = true;
-            nvimWebDevicons.enable = true;
-            indentBlankline = {
-              enable = true;
-              fillChar = null;
-              eolChar = null;
-              showCurrContext = true;
-            };
-            cursorWordline = {
-              enable = true;
-              lineTimeout = 0;
-            };
+            fillChar = null;
+            eolChar = null;
+            showCurrContext = true;
           };
-          statusline.lualine.enable = true;
-          theme.enable = true;
-          autopairs.enable = true;
-          autocomplete = {
+          cursorWordline = {
             enable = true;
-            type = "nvim-cmp";
+            lineTimeout = 0;
           };
-          filetree.nvimTreeLua = {
-            enable = true;
-            treeWidth = 500;
-            hideFiles = [];
-            disableNetRW = true;
-            closeOnFileOpen = true;
-          };
-          tabline.nvimBufferline.enable = true;
-          treesitter.context.enable = true;
-          keys = {
-            enable = true;
-            whichKey.enable = true;
-          };
-          telescope.enable = true;
-          git = {
-            enable = true;
-            gitsigns.enable = true;
-            gitsigns.codeActions = true;
-          };
+        };
+        statusline.lualine.enable = true;
+        theme.enable = true;
+        autopairs.enable = true;
+        autocomplete = {
+          enable = true;
+          type = "nvim-cmp";
+        };
+        filetree.nvimTreeLua = {
+          enable = true;
+          treeWidth = 500;
+          hideFiles = [];
+          disableNetRW = true;
+          closeOnFileOpen = true;
+        };
+        tabline.nvimBufferline.enable = true;
+        treesitter.context.enable = true;
+        keys = {
+          enable = true;
+          whichKey.enable = true;
+        };
+        telescope.enable = true;
+        git = {
+          enable = true;
+          gitsigns.enable = true;
+          gitsigns.codeActions = true;
+        };
         };
       };
     in {
